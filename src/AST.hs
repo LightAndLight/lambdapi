44ac52043acc8b3328983fc262fd732b4815bc51
Isaac Elliott <isaace71295@gmail.com>
2017-08-11 09:12:13 +1000

    Added int, char and string literals

--- src/AST.hs
+++ src/AST.hs
@@ -1,3 +1,4 @@
+{-# language PatternSynonyms #-}
 module AST where
 
 import Control.Applicative
@@ -12,7 +13,22 @@ data Expr
   | Free Name
   | App Expr Expr
   | Lam Expr
+  | Int Int
+  | String String
+  | Char Char
   deriving (Eq, Show)
+
+pattern Ctor :: String -> Expr
+pattern Ctor s = Free (Global s)
+
+pattern TInt :: Expr
+pattern TInt = Free (Global "Int")
+
+pattern TChar :: Expr
+pattern TChar = Free (Global "Char")
+
+pattern TString :: Expr
+pattern TString = Free (Global "String")
 
 data Name
   = Local Int
@@ -21,27 +37,30 @@ data Name
   deriving (Eq, Show)
 
 printExpr :: Expr -> String
-printExpr = go [] (liftA2 (++) (pure <$> ['a'..'z']) (show <$> [1::Int ..]))
+printExpr = go [] (("t"++) . show <$> [1::Int ..]) (("var"++) . show <$> [1::Int ..])
   where
-    go mapping supply e =
+    go mapping t_supply var_supply e =
       case e of
-        Ann a b -> "(" ++ go mapping supply a ++ ") : (" ++ go mapping supply b ++ ")"
+        Int n -> show n
+        String s -> show s
+        Char c -> show c
+        Ann a b -> "(" ++ go mapping t_supply var_supply a ++ ") : (" ++ go mapping t_supply var_supply b ++ ")"
         Type -> "Type"
         Pi a b ->
-          case supply of
-            name : supply' ->
-              "pi (" ++ name ++ " : " ++ go mapping supply a ++ "). " ++
-              go (name : mapping) supply' b
+          case t_supply of
+            name : t_supply' ->
+              "pi (" ++ name ++ " : " ++ go mapping t_supply' var_supply a ++ "). " ++
+              go (name : mapping) t_supply' var_supply b
             _ -> error "printExpr: name supply exhausted"
         Bound n -> mapping !! n
         Free (Global name) -> name
         Free a -> error "printExpr: unexpected " ++ show a ++ " in Free"
-        App f x -> "(" ++ go mapping supply f ++ ") (" ++ go mapping supply x ++ ")"
+        App f x -> "(" ++ go mapping t_supply var_supply f ++ ") (" ++ go mapping t_supply var_supply x ++ ")"
         Lam a -> 
-          case supply of
-            name : supply' ->
+          case var_supply of
+            name : var_supply' ->
               "lam " ++ name ++ ". " ++
-              go (name : mapping) supply' a
+              go (name : mapping) t_supply var_supply' a
             _ -> error "printExpr: name supply exhausted"
 
 data TypeError
@@ -60,25 +79,34 @@ data Value
   = VLam (Value -> Value)
   | VPi Value (Value -> Value)
   | VType
+  | VInt Int
+  | VChar Char
+  | VString String
   | VNeutral Neutral
 
 quote :: Int -> Value -> Expr
-quote binders v =
+quote i v =
   case v of
-    VLam f -> Lam . quote (binders+1) . f . VNeutral $ NVar $ Quote binders
-    VPi t f -> Pi (quote binders t) (quote (binders+1) . f . VNeutral $ NVar $ Quote binders)
+    VLam f -> Lam . quote (i+1) . f . VNeutral $ NVar $ Quote i
+    VPi t f -> Pi (quote i t) (quote (i+1) . f . VNeutral $ NVar $ Quote i)
     VType -> Type
-    VNeutral n -> fromNeutral binders n
+    VNeutral n -> fromNeutral i n
+    VInt n -> Int n
+    VChar c -> Char c
+    VString s -> String s
   where
-    fromNeutral binders (NVar (Quote n)) = Bound $ binders - n - 1
-    fromNeutral binders (NVar n) = Free n
-    fromNeutral binders (NApp n v) = App (fromNeutral binders n) (quote binders v)
+    fromNeutral i (NVar (Quote n)) = Bound $ i - n - 1
+    fromNeutral i (NVar n) = Free n
+    fromNeutral i (NApp n v) = App (fromNeutral i n) (quote i v)
   
 type Environment = [Value]
 
 eval :: Environment -> Expr -> Value
 eval ctxt expr =
   case expr of
+    Int n -> VInt n
+    String s -> VString s
+    Char c -> VChar c
     Ann e _ -> eval ctxt e
     Type -> VType
     Pi rho rho' -> VPi (eval ctxt rho) (\v -> eval (v : ctxt) rho')
@@ -94,18 +122,18 @@ eval ctxt expr =
 type Context = [(Name, Value)]
 
 check :: Int -> Context -> Expr -> Value -> Either TypeError ()
-check binders ctxt expr ty
+check i ctxt expr ty
   | Lam e <- expr =
       case ty of
         VPi tau tau' ->
           check
-            (binders+1)
-            ((Local binders, tau) : ctxt)
-            (subst 0 (Free $ Local binders) e)
-            (tau' . VNeutral $ NVar $ Local binders)
+            (i+1)
+            ((Local i, tau) : ctxt)
+            (subst 0 (Free $ Local i) e)
+            (tau' . VNeutral $ NVar $ Local i)
         _ -> Left $ NotAFunctionType $ quote 0 ty
   | otherwise = do
-      ty' <- infer binders ctxt expr
+      ty' <- infer i ctxt expr
       let
         v = quote 0 ty
         v' = quote 0 ty'
@@ -118,29 +146,34 @@ subst binder new orig
   | otherwise =
       case orig of
         Ann e t -> Ann (subst binder new e) t
-        Type -> Type
         Pi t e -> Pi (subst binder new t) (subst (binder+1) new e)
         Bound n -> if binder == n then new else orig
         App f x -> App (subst binder new f) (subst binder new x)
         Lam e -> Lam (subst (binder+1) new e)
-        Free a -> Free a
+        _ -> orig
 
 infer :: Int -> Context -> Expr -> Either TypeError Value
-infer binders ctxt expr =
+infer i ctxt expr =
   case expr of
+    Int _ -> pure . VNeutral . NVar $ Global "Int"
+    Char _ -> pure . VNeutral . NVar $ Global "Char"
+    String _ -> pure . VNeutral . NVar $ Global "String"
+    TInt -> pure VType
+    TChar -> pure VType
+    TString -> pure VType
     Ann e rho -> do
-      check binders ctxt rho VType
+      check i ctxt rho VType
       let tau = eval [] rho
-      check binders ctxt e tau
+      check i ctxt e tau
       pure tau
     Type -> pure VType
     Pi rho rho' -> do
-      check binders ctxt rho VType
+      check i ctxt rho VType
       let tau = eval [] rho
       check
-        (binders+1)
-        ((Local binders, tau) : ctxt)
-        (subst 0 (Free $ Local binders) rho')
+        (i+1)
+        ((Local i, tau) : ctxt)
+        (subst 0 (Free $ Local i) rho')
         VType
       pure VType
     Free name ->
@@ -149,10 +182,10 @@ infer binders ctxt expr =
         Nothing -> Left $ UnknownName name
     Bound a -> error $ "infer: unexpected " ++ show a
     App f x -> do
-      tf <- infer binders ctxt f
+      tf <- infer i ctxt f
       case tf of
         VPi tau tau' -> do
-          check binders ctxt x tau
+          check i ctxt x tau
           pure $ tau' (eval [] x)
         _ -> Left $ NotAFunction f $ quote 0 tf
     _ -> Left $ CannotInfer expr
